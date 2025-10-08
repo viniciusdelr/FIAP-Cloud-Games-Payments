683a4924c2b387ed68d957cdc8219f17fdbbb394
viniciusdelr <107009079+viniciusdelr@users.noreply.github.com>
2025-10-07 21:42:25 -0300

    feat: subindo commit geral

---
+++ FCG/src/FCG.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FCG.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+
+    }
+}
