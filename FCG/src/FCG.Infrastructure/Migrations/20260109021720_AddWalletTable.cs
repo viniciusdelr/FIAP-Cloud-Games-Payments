8cf05616e7b6a84ab8d2b719dff1210291a0eb71
Gabriel Palheiro <108705306+GabrielPalheiro@users.noreply.github.com>
2026-01-08 23:24:21 -0300

    feat: atualizando pacote

---
+++ FCG/src/FCG.Infrastructure/Migrations/20260109021720_AddWalletTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWalletTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wallets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Funds = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    LastRecharge = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wallets");
+        }
+    }
+}
